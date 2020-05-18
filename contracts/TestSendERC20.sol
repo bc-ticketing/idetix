d9b11de7c1058a67ab61c671fc5b7e9672362564
Simon <simibac93@gmail.com>
2020-05-18 14:40:54 +0200

    added contract for testing an ERC20 transfer from another account

--- contracts/TestSendERC20.sol
+++ contracts/TestSendERC20.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.4.22 <0.7.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract TestSendERC20 {
