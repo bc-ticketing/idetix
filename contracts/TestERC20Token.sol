822b0d250d4e154eac1cf2bceb30e24f2ade50cc
Simon <simibac93@gmail.com>
2020-05-18 12:10:36 +0200

    import for local dev adjusted

--- contracts/TestERC20Token.sol
+++ contracts/TestERC20Token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.7.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract TestERC20Token is ERC20 {
