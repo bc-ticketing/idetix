a46cce36c466654717c11ea8b4dd7f18377b9c95
Simon <simibac93@gmail.com>
2020-06-19 18:55:06 +0200

    removed EventFactory as we need an EventRegistery

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
 const SimpleStorage = artifacts.require("SimpleStorage");
-const EventFactory = artifacts.require("EventFactory");
 const TestERC20Token = artifacts.require("TestERC20Token");
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(EventFactory);
   deployer.deploy(TestERC20Token);
 };
