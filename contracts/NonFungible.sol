3f33b79fa97c40c2f54cc7579eb0b17c3db13e3b
Simon <simibac93@gmail.com>
2020-06-25 17:42:10 +0200

    Fungible tickets with new encoding + tests

--- contracts/NonFungible.sol
+++ contracts/NonFungible.sol
@@ -5,28 +5,36 @@ import './EventV3.sol';
 
 
 abstract contract NonFungible is EventV3{
-    event MintNonFungibles(address indexed owner, uint256 ticketType, uint256[] ids);
+    event MintNonFungibles(address indexed owner, uint256[] ids);
 
-    function mintNonFungibles(uint _type, uint256[] calldata _ids)
-        external payable
-        onlyNonFungible(_type)
-        onlyCorrectValue(_type, _ids.length)
+    function mintNonFungibles(uint256[] memory _ids)
+        public
+        payable
         onlyLessThanMaxTickets(msg.sender, _ids.length)
         onlyVerified(msg.sender)
     {
+        uint256 totalPrice = 0;
+
         for(uint256 i = 0; i<_ids.length; i++){
-            mintNonFungible(_type, _ids[i]);
+            totalPrice += mintNonFungible(_ids[i]);
         }
+
         totalTickets[msg.sender] = totalTickets[msg.sender].add(_ids.length);
-        owner.transfer(ticketTypeMeta[_type].price * _ids.length);
-        emit MintNonFungibles(msg.sender, _type, _ids);
+        require(totalPrice == msg.value, "The sent value does not match the total price.");
+
+        owner.transfer(totalPrice);
+        emit MintNonFungibles(msg.sender, _ids);
     }
 
-    function mintNonFungible(uint256 _type, uint256 _id)
-        internal
-        onlyMintableNfId(_type, _id)
+    function mintNonFungible(uint256 _id)
+        private
+        onlyNonMintedNf(_id)
+        returns(uint256 _price)
     {
-        nfTickets[_type][_id] = msg.sender;
+        // store how many nf tickets are owned by one account (maybe not needed)
+        tickets[getNonFungibleBaseType(_id)][msg.sender] += 1;
+        nfOwners[_id] = msg.sender;
+        return ticketTypeMeta[getNonFungibleBaseType(_id)].price;
     }
 }
 
--- test/NonFungible.js
+++ test/NonFungible.js
@@ -1,14 +1,36 @@
-const {cidToArgs, argsToCid} = require("../utils/ipfs-parser")
+const {cidToArgs, argsToCid} = require("../utils/ipfs-parser");
+const BigNumber = require('bignumber.js');
 
 const EventNonFungible = artifacts.require("EventNonFungible");
 
-contract("Fungible", (accounts) => {
+contract("NonFungible", (accounts) => {
   const cid = "QmWATWQ7fVPP2EFGu71UkfnqhYXDYH566qy47CnJDgvs8u";
   const args = cidToArgs(cid);
   const price = 1000;
-  const supply = 5;
+  const supply = 10;
   const isNF = true;
   const finalizationBlock = 1000;
+
+
+  // which is equivalent in binary to 1(126*0)1(128*0)
+  const ticketTypeId = new BigNumber("57896044618658097711785492504343953926975274699741220483192166611388333031424");
+  const ids = [
+    "57896044618658097711785492504343953926975274699741220483192166611388333031425",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031426",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031427",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031428",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031429",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031430",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031431",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031432",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031433",
+    "57896044618658097711785492504343953926975274699741220483192166611388333031434",
+  ];
+
+  const nonExistingIds = [
+    "57896044618658097711785492504343953926975274699741220483192166611388333031423"
+  ]
+
   let maxTicketsPerPerson = 0;
 
   let event = null;
@@ -22,7 +44,7 @@ contract("Fungible", (accounts) => {
       args.size,
       args.digest
     );
-    maxTicketsPerPerson = await event.maxTicketsPerPerson();
+    maxTicketsPerPerson = await event.maxTicketsPerPerson(); // default 2
   });
 
   it("should return the event smart contract", async () => {
@@ -43,7 +65,8 @@ contract("Fungible", (accounts) => {
       supply
     );
 
-    let ticketType = await event.ticketTypeMeta(0);
+    let ticketType = await event.ticketTypeMeta(ticketTypeId);
+    // console.log(ticketTypeId)
 
     assert.equal(
       ticketType["price"].toNumber(),
@@ -57,8 +80,6 @@ contract("Fungible", (accounts) => {
       "The supply is not set correctly."
     );
 
-    assert.equal(ticketType["isNF"], isNF, "The isNF is not set correctly.");
-
     assert.equal(
       ticketType["finalizationBlock"],
       finalizationBlock,
@@ -67,23 +88,23 @@ contract("Fungible", (accounts) => {
   });
 
   it("should mint 2 fungible tickets for acc0", async () => {
-    const ticketType = 0;
-    const ids = [0, 3];
+    const idsToBuy = [ids[0], ids[1]];
     var assignedOwner;
 
-    await event.mintNonFungibles(ticketType, ids, {
-      value: price * ids.length,
+    await event.mintNonFungibles(idsToBuy, {
+      value: price * idsToBuy.length,
       from: accounts[0],
     });
 
-    assignedOwner = await event.nfTickets(ticketType, ids[0]);
+
+    assignedOwner = await event.nfOwners(idsToBuy[0]);
     assert.equal(
       assignedOwner,
       accounts[0],
       "The ticket was assigned correctly"
     );
 
-    assignedOwner = await event.nfTickets(ticketType, ids[1]);
+    assignedOwner = await event.nfOwners(idsToBuy[1]);
     assert.equal(
       assignedOwner,
       accounts[0],
@@ -92,13 +113,12 @@ contract("Fungible", (accounts) => {
 
   });
 
-  it("should not allow minting more tickets than allowed", async () => {
-    const ids = [0,1,2,3,4];
-    const ticketType = 0;
+  it("should not allow acc1 minting more tickets than allowed", async () => {
+    const idsToBuy = [ids[2], ids[3], ids[4]];
 
     try {
-      await event.mintNonFungibles(ticketType, ids, {
-        value: price * ids.length,
+      await event.mintNonFungibles(idsToBuy, {
+        value: price * idsToBuy.length,
         from: accounts[1],
       });
       assert.fail("The transaction should have thrown an error");
@@ -108,14 +128,28 @@ contract("Fungible", (accounts) => {
     }
   });
 
-  it("should not allow minting tickets with an id that does not exist", async () => {
-    const unknownIds = [supply];
-    const ticketType = 0;
+  it("should not allow acc2 minting tickets with an id that does not exist", async () => {
+    const unknownIds = [nonExistingIds[0]];
 
     try {
-      await event.mintNonFungibles(ticketType, unknownIds, {
+      await event.mintNonFungibles(unknownIds, {
         value: price * unknownIds.length,
         from: accounts[2],
+      });
+      assert.fail("The transaction should have thrown an error");
+    }
+    catch (err) {
+      assert.include(err.message, "revert", "The error message should contain 'revert'");
+    }
+  });
+
+  it("should not allow acc3 minting tickets with the wrong value", async () => {
+    const idsToBuy = [ids[7], ids[8]];
+
+    try {
+      await event.mintNonFungibles(idsToBuy, {
+        value: price * idsToBuy.length -1 ,
+        from: accounts[3],
       });
       assert.fail("The transaction should have thrown an error");
     }
