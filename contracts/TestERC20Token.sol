c5fc1d4bbe01d565d24ede519517772691dd55e7
Simon <simibac93@gmail.com>
2020-07-26 12:06:45 +0200

    migration error fix

--- contracts/TestERC20Token.sol
+++ contracts/TestERC20Token.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Gold", "GLD") public {
-        _mint(msg.sender, initialSupply);
+contract TestERC20Token is ERC20 {
+    constructor(uint256 _initialSupply, string memory _name, string memory _nameTag) ERC20(_name, _nameTag) public {
+        _mint(msg.sender, _initialSupply);
     }
 }
